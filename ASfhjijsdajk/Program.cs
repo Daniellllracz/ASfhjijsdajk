cca73618e1b2cd3e25c139c097088b367ecfd5bd
Dániel Rácz <dracz898@gmail.com>
2023-09-12 12:21:32 +0200

    Add project files.

---
+++ ASfhjijsdajk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASfhjijsdajk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ ASfhjijsdajk/src/Sportolo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASfhjijsdajk.src
+{
+    class Sportolo
+    {
+        public string Nev { get; set; }
+        public bool Kategoria { get; set; }
+        public string Egyesulet { get; set; }
+
+        public int[] PontSzamok { get; set; }
+
+        public Sportolo(string r)
+        {
+            var v = r.Split(';');
+            Nev = v[0];
+            Kategoria = v[1] == "Felnott ferfi";
+            Egyesulet = v[2] == "n.a." ? null : v[2];
+            PontSzamok = new int[8];
+            for (int i = 0; i < PontSzamok.Length; i++)
+            {
+                PontSzamok[i] = int.Parse(v[i + 3]);
+
+            }
+        }
+
+    }
+}
